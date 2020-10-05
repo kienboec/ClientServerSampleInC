c161da7cf2b47ae1873094a2c9dab87f49e05abc
kienboeck <kienboec@technikum-wien.at>
2020-10-05 13:35:00 +0200

    added signalHandler

--- src/myclient.c
+++ src/myclient.c
@@ -20,6 +20,7 @@ int main(int argc, char **argv)
    char buffer[BUF];
    struct sockaddr_in address;
    int size;
+   int isQuit;
 
    ////////////////////////////////////////////////////////////////////////////
    // CREATE A SOCKET
@@ -39,11 +40,11 @@ int main(int argc, char **argv)
    memset(&address, 0, sizeof(address)); // init storage with 0
    address.sin_family = AF_INET;         // IPv4
    // https://man7.org/linux/man-pages/man3/htons.3.html
-   address.sin_port = htons(PORT);       
+   address.sin_port = htons(PORT);
    // https://man7.org/linux/man-pages/man3/inet_aton.3.html
    if (argc < 2)
    {
-      inet_aton("127.0.0.1", &address.sin_addr); 
+      inet_aton("127.0.0.1", &address.sin_addr);
    }
    else
    {
@@ -53,45 +54,117 @@ int main(int argc, char **argv)
    ////////////////////////////////////////////////////////////////////////////
    // CREATE A CONNECTION
    // https://man7.org/linux/man-pages/man2/connect.2.html
-   if (connect(create_socket, 
-               (struct sockaddr *)&address, 
-               sizeof(address)) == 0)
-   {
-      printf("Connection with server (%s) established\n", 
-             inet_ntoa(address.sin_addr));
-
-
-      /////////////////////////////////////////////////////////////////////////
-      // RECEIVE DATA
-      // https://man7.org/linux/man-pages/man2/recv.2.html
-      size = recv(create_socket, buffer, BUF - 1, 0);
-      if (size > 0)
-      {
-         buffer[size] = '\0';
-         printf("%s", buffer);
-      }
-   }
-   else
+   if (connect(create_socket,
+               (struct sockaddr *)&address,
+               sizeof(address)) == -1)
    {
       // https://man7.org/linux/man-pages/man3/perror.3.html
       perror("Connect error - no server available");
       return EXIT_FAILURE;
    }
 
+   // ignore return value of printf
+   printf("Connection with server (%s) established\n",
+          inet_ntoa(address.sin_addr));
+
+   ////////////////////////////////////////////////////////////////////////////
+   // RECEIVE DATA
+   // https://man7.org/linux/man-pages/man2/recv.2.html
+   size = recv(create_socket, buffer, BUF - 1, 0);
+   if (size == -1)
+   {
+      perror("recv error");
+   }
+   else if (size == 0)
+   {
+      printf("Server closed remote socket\n"); // ignore error
+   }
+   else
+   {
+      buffer[size] = '\0';
+      printf("%s", buffer); // ignore error
+   }
+
    do
    {
-      printf("Send message: ");
+      printf(">> ");
       if (fgets(buffer, BUF, stdin) != NULL)
       {
+         int size = strlen(buffer);
+         // remove new-line signs from string at the end
+         if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
+         {
+            size -= 2;
+            buffer[size] = 0;
+         }
+         else if (buffer[size - 1] == '\n')
+         {
+            --size;
+            buffer[size] = 0;
+         }
+         isQuit = strcmp(buffer, "quit") == 0;
+
          //////////////////////////////////////////////////////////////////////
          // SEND DATA
          // https://man7.org/linux/man-pages/man2/send.2.html
-         send(create_socket, buffer, strlen(buffer), 0);
+         // send will fail if connection is closed, but does not set
+         // the error of send, but still the count of bytes sent
+         if ((send(create_socket, buffer, size, 0)) == -1) 
+         {
+            // in case the server is gone offline we will still not enter
+            // this part of code: see docs: https://linux.die.net/man/3/send
+            // >> Successful completion of a call to send() does not guarantee 
+            // >> delivery of the message. A return value of -1 indicates only 
+            // >> locally-detected errors.
+            // ... but
+            // to check the connection before send is sense-less because
+            // after checking the communication can fail (so we would need
+            // to have 1 atomic operation to check...)
+            perror("send error");
+            break;
+         }
+
+         //////////////////////////////////////////////////////////////////////
+         // RECEIVE FEEDBACK
+         size = recv(create_socket, buffer, BUF - 1, 0);
+         if (size == -1)
+         {
+            perror("recv error");
+            break;
+         }
+         else if (size == 0)
+         {
+            printf("Server closed remote socket\n"); // ignore error
+            break;
+         }
+         else
+         {
+            buffer[size] = '\0';
+            printf("<< %s\n", buffer); // ignore error
+            if (strcmp("OK", buffer) != 0)
+            {
+               fprintf(stderr, "<< Server error occured, abort\n");
+               break;
+            }
+         }
       }
-   } while (strcmp(buffer, "quit\n") != 0);
+   } while (!isQuit);
 
    ////////////////////////////////////////////////////////////////////////////
    // CLOSES THE DESCRIPTOR
-   close(create_socket);
+   if (create_socket != -1)
+   {
+      if (shutdown(create_socket, SHUT_RDWR) == -1)
+      {
+         // invalid in case the server is gone already
+         perror("shutdown create_socket"); 
+      }
+      if (close(create_socket) == -1)
+      {
+         perror("close create_socket");
+      }
+      create_socket = -1;
+   }
+
    return EXIT_SUCCESS;
 }
--- src/myserver.c
+++ src/myserver.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -14,13 +15,32 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
+int abortRequested = 0;
+int create_socket = -1;
+int new_socket = -1;
+
+///////////////////////////////////////////////////////////////////////////////
+
+void *clientCommunication(void *data);
+void signalHandler(int sig);
+
+///////////////////////////////////////////////////////////////////////////////
+
 int main(void)
 {
-   int create_socket, new_socket;
-   char buffer[BUF];
-   int size;
    socklen_t addrlen;
    struct sockaddr_in address, cliaddress;
+   int reuseValue = 1;
+
+   ////////////////////////////////////////////////////////////////////////////
+   // SIGNAL HANDLER
+   // SIGINT (Interrup: ctrl+c)
+   // https://man7.org/linux/man-pages/man2/signal.2.html
+   if (signal(SIGINT, signalHandler) == SIG_ERR)
+   {
+      perror("signal can not be registered");
+      return EXIT_FAILURE;
+   }
 
    ////////////////////////////////////////////////////////////////////////////
    // CREATE A SOCKET
@@ -28,9 +48,34 @@ int main(void)
    // https://man7.org/linux/man-pages/man7/ip.7.html
    // https://man7.org/linux/man-pages/man7/tcp.7.html
    // IPv4, TCP (connection oriented), IP (same as client)
-   if((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+   if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
    {
       perror("Socket error"); // errno set by socket()
+      return EXIT_FAILURE;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // SET SOCKET OPTIONS
+   // https://man7.org/linux/man-pages/man2/setsockopt.2.html
+   // https://man7.org/linux/man-pages/man7/socket.7.html
+   // socket, level, optname, optvalue, optlen
+   if (setsockopt(create_socket,
+                  SOL_SOCKET,
+                  SO_REUSEADDR,
+                  &reuseValue,
+                  sizeof(reuseValue)) == -1)
+   {
+      perror("set socket options - reuseAddr");
+      return EXIT_FAILURE;
+   }
+
+   if (setsockopt(create_socket,
+                  SOL_SOCKET,
+                  SO_REUSEPORT,
+                  &reuseValue,
+                  sizeof(reuseValue)) == -1)
+   {
+      perror("set socket options - reusePort");
       return EXIT_FAILURE;
    }
 
@@ -44,7 +89,7 @@ int main(void)
 
    ////////////////////////////////////////////////////////////////////////////
    // ASSIGN AN ADDRESS WITH PORT TO SOCKET
-   if (bind(create_socket, (struct sockaddr *)&address, sizeof(address)) != 0)
+   if (bind(create_socket, (struct sockaddr *)&address, sizeof(address)) == -1)
    {
       perror("bind error");
       return EXIT_FAILURE;
@@ -53,64 +98,180 @@ int main(void)
    ////////////////////////////////////////////////////////////////////////////
    // ALLOW CONNECTION ESTABLISHING
    // Socket, Backlog (= count of waiting connections allowed)
-   listen(create_socket, 5);
-
-
-   while (1)
+   if (listen(create_socket, 5) == -1)
    {
+      perror("listen error");
+      return EXIT_FAILURE;
+   }
+
+   while (!abortRequested)
+   {
+      /////////////////////////////////////////////////////////////////////////
+      // ignore errors here... because only information message
+      // https://linux.die.net/man/3/printf
       printf("Waiting for connections...\n");
 
       /////////////////////////////////////////////////////////////////////////
       // ACCEPTS CONNECTION SETUP
-      // blocking
+      // blocking, might have an accept-error on ctrl+c
       addrlen = sizeof(struct sockaddr_in);
-      new_socket = accept(create_socket, 
-                          (struct sockaddr *)&cliaddress, 
-                          &addrlen);
-      if (new_socket > 0)
+      if ((new_socket = accept(create_socket,
+                               (struct sockaddr *)&cliaddress,
+                               &addrlen)) == -1)
       {
-         printf("Client connected from %s:%d...\n", 
-                inet_ntoa(cliaddress.sin_addr), 
-                ntohs(cliaddress.sin_port));
-         strcpy(buffer, "Welcome to myserver!\r\nPlease enter your commands...\r\n");
-         send(new_socket, buffer, strlen(buffer), 0);
+         if (abortRequested)
+         {
+            perror("accept error after aborted");
+         }
+         else
+         {
+            perror("accept error");
+         }
+         break;
       }
 
-      // think here about thread-handling or forking ...
+      /////////////////////////////////////////////////////////////////////////
+      // START CLIENT
+      // ignore printf error handling
+      printf("Client connected from %s:%d...\n",
+             inet_ntoa(cliaddress.sin_addr),
+             ntohs(cliaddress.sin_port));
+      clientCommunication(&new_socket); // returnValue can be ignored
+      new_socket = -1;
+   }
 
-      do
+   // frees the descriptor
+   if (create_socket != -1)
+   {
+      if (shutdown(create_socket, SHUT_RDWR) == -1)
       {
-         //////////////////////////////////////////////////////////////////////
-         // RECEIVE
-         size = recv(new_socket, buffer, BUF - 1, 0);
-         if (size > 0)
-         {
-            // remove ugly debug message, because of the sent newline
-            if(buffer[size-1] == '\n') {
-               --size;
-            }
+         perror("shutdown create_socket");
+      }
+      if (close(create_socket) == -1)
+      {
+         perror("close create_socket");
+      }
+      create_socket = -1;
+   }
 
-            buffer[size] = '\0';
-            printf("Message received: %s\n", buffer);
-         }
-         else if (size == 0)
+   return EXIT_SUCCESS;
+}
+
+void *clientCommunication(void *data)
+{
+   char buffer[BUF];
+   int size;
+   int *current_socket = (int *)data;
+
+   ////////////////////////////////////////////////////////////////////////////
+   // SEND welcome message
+   strcpy(buffer, "Welcome to myserver!\r\nPlease enter your commands...\r\n");
+   if (send(*current_socket, buffer, strlen(buffer), 0) == -1)
+   {
+      perror("send failed");
+      return NULL;
+   }
+
+   do
+   {
+      /////////////////////////////////////////////////////////////////////////
+      // RECEIVE
+      size = recv(*current_socket, buffer, BUF - 1, 0);
+      if (size == -1)
+      {
+         if (abortRequested)
          {
-            printf("Client closed remote socket\n");
-            break;
+            perror("recv error after aborted");
          }
          else
          {
             perror("recv error");
-            return EXIT_FAILURE;
          }
+         break;
+      }
 
-      } while (strcmp(buffer, "quit") != 0);
+      if (size == 0)
+      {
+         printf("Client closed remote socket\n"); // ignore error
+         break;
+      }
 
-      // frees the descriptor
-      close(new_socket);
+      // remove ugly debug message, because of the sent newline of client
+      if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n')
+      {
+         size -= 2;
+      }
+      else if (buffer[size - 1] == '\n')
+      {
+         --size;
+      }
+
+      buffer[size] = '\0';
+      printf("Message received: %s\n", buffer); // ignore error
+
+      if (send(*current_socket, "OK", 3, 0) == -1)
+      {
+         perror("send answer failed");
+         return NULL;
+      }
+   } while (strcmp(buffer, "quit") != 0 && !abortRequested);
+
+   // closes/frees the descriptor if not already
+   if (*current_socket != -1)
+   {
+      if (shutdown(*current_socket, SHUT_RDWR) == -1)
+      {
+         perror("shutdown new_socket");
+      }
+      if (close(*current_socket) == -1)
+      {
+         perror("close new_socket");
+      }
+      *current_socket = -1;
    }
 
-   // frees the descriptor
-   close(create_socket);
-   return EXIT_SUCCESS;
+   return NULL;
+}
+
+void signalHandler(int sig)
+{
+   if (sig == SIGINT)
+   {
+      printf("abort Requested... "); // ignore error
+      abortRequested = 1;
+      /////////////////////////////////////////////////////////////////////////
+      // With shutdown() one can initiate normal TCP close sequence ignoring
+      // the reference count.
+      // https://beej.us/guide/bgnet/html/#close-and-shutdownget-outta-my-face
+      // https://linux.die.net/man/3/shutdown
+      if (new_socket != -1)
+      {
+         if (shutdown(new_socket, SHUT_RDWR) == -1)
+         {
+            perror("shutdown new_socket");
+         }
+         if (close(new_socket) == -1)
+         {
+            perror("close new_socket");
+         }
+         new_socket = -1;
+      }
+
+      if (create_socket != -1)
+      {
+         if (shutdown(create_socket, SHUT_RDWR) == -1)
+         {
+            perror("shutdown create_socket");
+         }
+         if (close(create_socket) == -1)
+         {
+            perror("close create_socket");
+         }
+         create_socket = -1;
+      }
+   }
+   else
+   {
+      exit(sig);
+   }
 }
